b89fd7490f2e7ebac78158abd5fb487eaed3e8fb
Sam Popowich <spopowich@gmail.com>
2014-09-29 14:33:42 -0600

    Add names parser.

--- lib/nlp_parser.rb
+++ lib/nlp_parser.rb
@@ -30,4 +30,37 @@ class NLPParser
     tagger = OpenNLP::POSTaggerME.new
     tagger.tag(tokens).to_a
   end
+
+  def names
+    @names || get_names
+  end
+
+  def get_names
+    OpenNLP.load
+    tokenizer   = OpenNLP::TokenizerME.new
+    segmenter   = OpenNLP::SentenceDetectorME.new
+    ner_models  = ['person']
+
+    ner_finders = ner_models.map do |model|
+      OpenNLP::NameFinderME.new("en-ner-#{model}.bin")
+    end
+
+    sentences = segmenter.sent_detect(text)
+    named_entities = []
+
+    sentences.each do |sentence|
+      tokens = tokenizer.tokenize(sentence)
+      ner_models.each_with_index do |model,i|
+        finder = ner_finders[i]
+        name_spans = finder.find(tokens)
+        name_spans.each do |name_span|
+          start = name_span.get_start
+          stop  = name_span.get_end-1
+          slice = tokens[start..stop].to_a
+          named_entities << [slice, model]
+        end
+      @names = named_entities
+      end
+    end
+  end
 end
