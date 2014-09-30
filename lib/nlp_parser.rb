fb73bfee77607e6f1c75bb88cf51927c11ea70e6
Sam Popowich <spopowich@gmail.com>
2014-09-30 09:57:35 -0600

    Finish refactor.

--- lib/nlp_parser.rb
+++ lib/nlp_parser.rb
@@ -1,12 +1,12 @@
 require "open-uri"
 require "open-nlp"
-require_relative "./mods_article.rb"
 
 class NLPParser
 
   def initialize(article_class)
     @article = article_class
     OpenNLP.load
+    @tokenizer = OpenNLP::TokenizerME.new
   end
 
   def from_xml xml_record
@@ -18,12 +18,11 @@ class NLPParser
   end
 
   def tokens
-    @tokens || tokenize
+    tokenize text
   end
 
-  def tokenize
-    tokenizer = OpenNLP::TokenizerME.new
-    @tokens = tokenizer.tokenize(text)
+  def tokenize text
+    @tokenizer.tokenize(text)
   end
 
   def tags
@@ -36,31 +35,42 @@ class NLPParser
   end
 
   def extracted_names
-    tokenizer   = OpenNLP::TokenizerME.new
     segmenter   = OpenNLP::SentenceDetectorME.new
-    ner_models  = ['person']
+    @extracted_names = []
 
-    ner_finders = ner_models.map do |model|
-      OpenNLP::NameFinderME.new("en-ner-#{model}.bin")
-
+    segmenter.sent_detect(text).each do |sentence|
+      tokens = tokenize(sentence)
+      find_names_in(tokens)
     end
-    sentences = segmenter.sent_detect(text)
-    names = []
+    @extracted_names
+  end
 
-    sentences.each do |sentence|
-      tokens = tokenizer.tokenize(sentence)
-      ner_models.each_with_index do |model,i|
-        finder = ner_finders[i]
-        name_spans = finder.find(tokens)
-        name_spans.each do |name_span|
-          start = name_span.get_start
-          stop  = name_span.get_end-1
-          slice = tokens[start..stop].to_a
-          names << slice.join("_")
-        end
-      end
+  def find_names_in(tokens)
+    ner_models  = ['person'] # add more models to find more entity-types
+    ner_finders = populate_finders_for ner_models
+    ner_models.each_with_index do |model,i|
+      find_names(ner_finders[i], tokens)
     end
-    names
+  end
+
+  def find_names(finder, tokens)
+    name_spans = finder.find(tokens)
+    name_spans.each do |name_span|
+      @extracted_names << slice(name_span, tokens)
+    end
+  end
+
+  def slice(name_span, tokens)
+      start = name_span.get_start
+      stop  = name_span.get_end-1
+      slice = tokens[start..stop].to_a
+      slice.join("_")
+  end
+
+  def populate_finders_for ner_models
+    ner_models.map do |model|
+       OpenNLP::NameFinderME.new("en-ner-#{model}.bin")
+    end
   end
 
   def uris
